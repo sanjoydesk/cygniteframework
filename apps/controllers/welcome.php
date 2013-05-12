363c9d420d2bd59b38e69bd730072ceadc4bee8b
Sanjoy Dey <sanjoy09@hotmail.com>
2013-05-12 13:10:42 +0530

    Configuration changes on config file

--- apps/controllers/welcome.php
+++ apps/controllers/welcome.php
@@ -1,32 +1,30 @@
-<?php if ( ! defined('CF_BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('CF_BASEPATH'))
+    exit('No direct script access allowed');
 /*
-*===============================================================================================
-*
-* ===============================================================================================
-*/
-class WelcomeAppsController extends CF_ApplicationController
-{
-        function __construct()
-        {
+ * ===============================================================================================
+ *
+ * ===============================================================================================
+ */
+    class WelcomeAppsController extends CF_ApplicationController {
+
+        function __construct() {
             parent::__construct();
             $this->app()->model('DBmodel');
-
         }
 
-        public function action_index()
-        {
-            $data['userdetails']=  $this->app()->DBmodel->getdetails();
+        public function action_index() {
+            $data['userdetails'] = $this->app()->DBmodel->getdetails();
             $data['values'] = "Sanjay";
-            $this->app()->render("user_details_view",$data);
+            $this->app()->render("user_details_view", $data);
         }
-        public function action_userlist()
-        {
+
+        public function action_userlist() {
             echo "This is userlist";
         }
-        public function action_testing()
-        {
-                echo "I am in ".__METHOD__;
 
+        public function action_testing() {
+            echo "I am in " . __METHOD__;
         }
 
-}
+    }

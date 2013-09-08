047afe14bcf1e2e3366d2f22b22168e168b191dc
Mr. Sanjoy Dey <sanjoy09@hotmail.com>
2013-09-08 17:15:27 +0530

    Update config.php

--- packages/cygnite/helpers/config.php
+++ packages/cygnite/helpers/config.php
@@ -3,80 +3,79 @@ namespace Cygnite\Helpers;
 
 if ( ! defined('CF_SYSTEM')) exit('No External script access allowed');
 /**
- *  Cygnite Framework
- *
- *  An open source application development framework for PHP 5.3x or newer
- *
- *   License
- *
- *   This source file is subject to the MIT license that is bundled
- *   with this package in the file LICENSE.txt.
- *   http://www.cygniteframework.com/license.txt
- *   If you did not receive a copy of the license and are unable to
- *   obtain it through the world-wide-web, please send an email
- *   to sanjoy@hotmail.com so I can send you a copy immediately.
- *
- * @Package                         :  Packages
- * @Sub Packages               :  Library
- * @Filename                       :  CF_Config
- * @Description                   : This file is used to load all framework configurations via Registry and store it in order to use it later.
- * @Author                           : Cygnite dev team
- * @Copyright                     :  Copyright (c) 2013 - 2014,
- * @Link	                  :  http://www.cygniteframework.com
- * @Since	                  :  Version 1.0
- * @Filesource
- * @Warning                     :  Any changes in this library can cause abnormal behaviour of the framework
- *
- *
- */
+*  Cygnite Framework
+*
+*  An open source application development framework for PHP 5.3x or newer
+*
+*   License
+*
+*   This source file is subject to the MIT license that is bundled
+*   with this package in the file LICENSE.txt.
+*   http://www.cygniteframework.com/license.txt
+*   If you did not receive a copy of the license and are unable to
+*   obtain it through the world-wide-web, please send an email
+*   to sanjoy@hotmail.com so I can send you a copy immediately.
+*
+* @Package              :  Packages
+* @Sub Package          :  Library
+* @Filename             :  CF_Config
+* @Description          : This file is used to load all framework configurations via Registry and store it in order to use it later.
+* @Author               : Cygnite dev team
+* @Copyright            :  Copyright (c) 2013 - 2014,
+* @Link	             :  http://www.cygniteframework.com
+* @Since	             :  Version 1.0
+* @Filesource
+* @Warning              :  Any changes in this library can cause abnormal behaviour of the framework
+*
+*
+*/
     $config = array();
-   $config = Config::appsconfig();
+    $config = Config::appsconfig(); //Get the configuration variables
     Config::store_config_items('config_items',$config);
     unset($config);
-     //Get the configuration variables
-    Url::set_basepath(Config::getconfig('global_config','base_path'));
+    Url::setBase(Config::getconfig('global_config','base_path'));//Set URL base path
 
     class Config
     {
-            private static $config = array();
+        private static $config = array();
+    
+        public static function getconfig($arrkey,$keyval = FALSE)
+        {
+            $config = array();
 
-            public static function getconfig($arrkey,$keyval = FALSE)
-            {
-                    $config = array();
+            $config =  Config::get_config_items('config_items');
 
-                    $config =  Config::get_config_items('config_items');
+            if(is_null($arrkey))
+                   throw new InvalidArgumentException("Cannot pass null argument to ".__METHOD__);
 
-                    if(is_null($arrkey))
-                           throw new InvalidArgumentException("Cannot pass null argument to ".__METHOD__);
+             if(FALSE !== array_key_exists($arrkey, $config) && $keyval == FALSE)
+                        return $config[$arrkey];
 
-                     if(FALSE !== array_key_exists($arrkey, $config) && $keyval == FALSE)
-                                return $config[$arrkey];
-
-                    if(FALSE !== array_key_exists($arrkey, $config) && $keyval != FALSE)
-                          return $config[$arrkey][$keyval];
-
-            }
-
-            public static function store_config_items($name, $values = array())
-            {
-                  self::$config[$name]  = $values;
-            }
-
-            public static function get_config_items($key)
-            {
-                if(is_null($key))
-                           throw new InvalidArgumentException("Cannot pass null argument to ".__METHOD__);
-              return self::$config[strtolower($key)];
-            }
-
-            public static function appsconfig()
-            {
-                  $config = array();
-                 $config['global_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'config'.EXT;
-                 $config['db_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'database'.EXT;
-                 $config['session_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'session'.EXT;
-                 $config['autoload_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'autoload'.EXT;
-                 $config['routing_config'] = include_once strtolower(APPPATH).DS.'routerconfig'.EXT;
-                 return $config;
-            }
+            if(FALSE !== array_key_exists($arrkey, $config) && $keyval != FALSE)
+                  return $config[$arrkey][$keyval];
+    
+        }
+    
+        public static function store_config_items($name, $values = array())
+        {
+              self::$config[$name]  = $values;
+        }
+    
+        public static function get_config_items($key)
+        {
+            if(is_null($key))
+              throw new InvalidArgumentException("Cannot pass null argument to ".__METHOD__);
+            return self::$config[strtolower($key)];
+        }
+    
+        public static function appsconfig()
+        {
+             $config = array();
+             $config['global_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'config'.EXT;
+             $config['db_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'database'.EXT;
+             $config['session_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'session'.EXT;
+             $config['autoload_config'] = include_once strtolower(APPPATH).DS.'configs'.DS.'autoload'.EXT;
+             $config['routing_config'] = include_once strtolower(APPPATH).DS.'routerconfig'.EXT;
+             return $config;
+        }
     }

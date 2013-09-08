ce082e1586d5a3f14facaa7b715a6b5ad7ceddbe
Mr. Sanjoy Dey <sanjoy09@hotmail.com>
2013-09-08 16:57:57 +0530

    Update welcomeuser.php
    Index method update for user authentication.

--- apps/controllers/welcomeuser.php
+++ apps/controllers/welcomeuser.php
@@ -45,21 +45,36 @@ use Cygnite\Helpers\GHelper;
         public function action_index()
         {
 
-            $isAuthenticated = Cygnite::loader()->CFAuthx->identifyUser(new \Apps\Components\Libs\AuthxIdentity(
-                                                array(
-                                                      'username' => 'sanjay',//post values need to be passed for username or email address field
-                                                      'password' =>  'sanjay2123',
-                                                     // 'status' => 1 // optional field to check user authentication
-                                                     )
-                                                )
-                                                );
-
-               if($isAuthenticated === TRUE):
-                      echo "User Authenticated Successfully";
-               else:
-                       echo "Not a valid User";
-               endif;
-
+            /*
+            $model = new \Apps\Models\Activerecords('cygnite');
+            $model->name =    "Cygnite Framework Active Records !!";
+            $model->entry_date = date('Y-m-d H:m:s');
+            $model->comment =  'ORM type insert http://www.cygniteframework.com';
+            $model->save('guestbook'); 
+            */
+            $this->authConfig = new AuthxIdentity(array(
+                                          'username' => 'sanjay',//post values need to be passed for username or email address field
+                                          'password' =>  'sanjay2123', // your password
+                                         // 'status' => 1 // optional field to check user authentication
+                                         )
+                               );
+            
+            if(Cygnite::loader()->authx->identifyUser($this->authConfig))
+                echo "User Authenticated Successfully";
+            else
+                echo "Not a valid User";
+            
+            $this->createsections(array(
+                               'header'=>'layout:header.view',
+                               'content'=>'welcomeuser:register.view',
+                               'footer'=>'layout:footer.view'
+                               ));
+            $this->setlayout('layout:admin',array(
+                                    'title'=> "Welcome Sanjoy",
+                                    'header_title'=> "Header Page",
+                                    'content_title'=> "Content Page",
+                                    'footer_title'=> "Footer Page"
+                                    ));
 
         }
   }

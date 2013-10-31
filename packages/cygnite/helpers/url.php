9c9e29980de9d97ef3f91ea680ff6d73ff90342f
Eric Clinton <rohitlovely87@gmail.com>
2013-10-31 10:02:12 +0530

    Update url.php
    Code has been aligned.

--- packages/cygnite/helpers/url.php
+++ packages/cygnite/helpers/url.php
@@ -1,4 +1,5 @@
-<?php namespace Cygnite\Helpers;
+<?php
+namespace Cygnite\Helpers;
 
 use InvalidArgumentException;
 
@@ -35,35 +36,44 @@ class Url
     public static $base;
 
     /**
-    * Header Redirect
-    *
-    * @access    public
-    * @false string $uri
-    * @false string $type
-    * @false int    $http_response_code
-    * @internal  false \Cygnite\Helpers\the $string URL
-    * @internal  false \Cygnite\Helpers\the $string method: location or redirect
-    * @return    string
-    */
+     * Header Redirect
+     *
+     * @access    public
+     * @false     string $uri
+     * @false     string $type
+     * @false     int    $http_response_code
+     * @internal  false \Cygnite\Helpers\the $string URL
+     * @internal  false \Cygnite\Helpers\the $string method: location or redirect
+     * @param string $uri
+     * @param string $type
+     * @param int    $http_response_code
+     * @return    string
+     */
     public static function redirectTo($uri = '', $type = 'location', $http_response_code = 302)
     {
 
-        $uri = str_replace(array('.','/'),'/',$uri);
-            if (! preg_match('#^https?://#i', $uri))
-                  $uri = self::sitePath($uri);
-           if($type == 'refresh')
-                    header("Refresh:0;url=".$uri);
-           else
-                    header("Location: ".$uri, true, $http_response_code);
+        $uri = str_replace(array('.', '/'), '/', $uri);
 
-        $uri = str_replace(array('.','/'),'/',$uri);
-
-        if (! preg_match('#^https?://#i', $uri))
+        if (! preg_match('#^https?://#i', $uri)) {
             $uri = self::sitePath($uri);
-        if($type == 'refresh')
+        }
+
+        if ($type == 'refresh') {
+            header("Refresh:0;url=".$uri);
+        } else {
+            header("Location: ".$uri, true, $http_response_code);
+        }
+
+        $uri = str_replace(array('.', '/'), '/', $uri);
+
+        if (!preg_match('#^https?://#i', $uri)) {
+            $uri = self::sitePath($uri);
+        }
+        if ($type == 'refresh') {
               header("Refresh:0;url=".$uri);
-        else
+        } else {
               header("Location: ".$uri, true, $http_response_code);
+        }
 
     }
     /**
@@ -72,19 +82,20 @@ class Url
     * @access public
     * @return string
     */
-    public function referedFrom()
+    public function referredFrom()
     {
 
     }
 
-   /**
-    * This Function is to get Uri Segment of the url
-    *
-    * @access public
-    * @false  int
-    * @return string
-    */
-    public static function segment($segment=1)
+    /**
+     * This Function is to get Uri Segment of the url
+     *
+     * @access public
+     * @false  int
+     * @param int $segment
+     * @return string
+     */
+    public static function segment($segment = 1)
     {
         // Current Request URI
         $uri = $_SERVER['REQUEST_URI'];
@@ -94,82 +105,88 @@ class Url
         $uri = substr($uri, strlen($basepath));
 
         // Don't take query falses into account on the URL
-        if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+        if (strstr($uri, '?')) {
+            $uri = substr($uri, 0, strpos($uri, '?'));
+        }
 
         // Remove trailing slash + enforce a slash at the start
          $uri = '/' . trim($uri, '/');
 
-       $urlArray = array_filter(explode('/', $uri));
+        $urlArray = array_filter(explode('/', $uri));
 
-       $indexCount = array_search('index.php',$urlArray);
+        $indexCount = array_search('index.php', $urlArray);
 
-       if($indexCount==true)
-              return @$urlArray[$indexCount+$segment];
-       else
-              return @$urlArray[$segment];
+        if ($indexCount == true) {
+            return @$urlArray[$indexCount+$segment];
+        } else {
+            return @$urlArray[$segment];
+        }
 
     }
 
 
-    static public function __callStatic($method, $args)
+    public static function __callStatic($method, $args)
     {
-        $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? 'https://' : 'http://';
 
-          if (preg_match('/^([gs]et)([A-Z])(.*)$/', $method, $match)) :
-                    $reflector = new \ReflectionClass(__CLASS__);
+        if (preg_match('/^([gs]et)([A-Z])(.*)$/', $method, $match)) {
+            $reflector = new \ReflectionClass(__CLASS__);
 
-                    $property = strtolower($match[2]). $match[3];
+            $property = strtolower($match[2]). $match[3];
 
-                    if ($reflector->hasProperty($property)) :
-                         $property = $reflector->getProperty($property);
+            if ($reflector->hasProperty($property)) {
+                 $property = $reflector->getProperty($property);
 
-                               switch ($match[1]):
-                                        case 'get':
-                                            return $protocol.$_SERVER['HTTP_HOST'].$property->getValue();
-                                        case 'set':
-                                            return $protocol.$_SERVER['HTTP_HOST'].$property->setValue($args[0]);
-                               endswitch;
-                    else:
-                            throw new InvalidArgumentException("Url Property {$property} doesn't exist");
-                    endif;
-           endif;
+                switch ($match[1]) {
+                    case 'get':
+                        return $protocol.$_SERVER['HTTP_HOST'].$property->getValue();
+                    case 'set':
+                        return $protocol.$_SERVER['HTTP_HOST'].$property->setValue($args[0]);
+                }
+            } else {
+                    throw new InvalidArgumentException("Url Property {$property} doesn't exist");
+            }
+        }
     }
 
     /**
-    * This Function is to get the url sitePath with index.php
-    *
-    * @access public
-    * @false $uri
-    * @return string
-    */
+     * This Function is to get the url sitePath with index.php
+     *
+     * @access public
+     * @false $uri
+     * @param $uri
+     * @return string
+     */
     public static function sitePath($uri)
     {
-        $expression = array_filter(explode('/',($_SERVER['REQUEST_URI'])));
-        $find_index = array_search('index.php',$expression);
-        $index = (false !== array_search('index.php',$expression)) ? 'index.php/' : '';
+        $expression = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+        $index = (false !== array_search('index.php', $expression)) ? 'index.php/' : '';
         
         return  Url::getBase().$index.$uri;
     }
 
 
     /**
-    * This Function is to encode the url
-    *
-    * @access public
-    * @false  string
-    * @return string
-    */
+     * This Function is to encode the url
+     *
+     * @access public
+     * @false  string
+     * @param $str
+     * @return string
+     */
     public static function encode($str)
     {
-          return urlencode($str);
+        return urlencode($str);
     }
-     /**
-    * This Function is to decode the url
-    *
-    * @access public
-    * @false  string
-    * @return string
-    */
+
+    /**
+     * This Function is to decode the url
+     *
+     * @access public
+     * @false  string
+     * @param $str
+     * @return string
+     */
     public static function decode($str)
     {
          return urldecode($str);

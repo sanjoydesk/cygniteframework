8a9808a6f6ca61010b8ee87a6be3796589362390
Sanjoy Dey <sanjoy09@hotmail.com>
2013-10-31 09:48:22 +0530

    Update ghelper.php
    Code aligned properly

--- packages/cygnite/helpers/ghelper.php
+++ packages/cygnite/helpers/ghelper.php
@@ -1,8 +1,11 @@
-<?php namespace Cygnite\Helpers;
+<?php
+namespace Cygnite\Helpers;
 
 use Cygnite\Cygnite;
 
-if ( ! defined('CF_SYSTEM')) exit('External script access not allowed');
+if (!defined('CF_SYSTEM')) {
+    exit('External script access not allowed');
+}
 /**
  *  Cygnite Framework
  *
@@ -17,68 +20,73 @@ if ( ! defined('CF_SYSTEM')) exit('External script access not allowed');
  *   obtain it through the world-wide-web, please send an email
  *   to sanjoy@hotmail.com so I can send you a copy immediately.
  *
- * @Package                         :  Packages
- * @Sub Packages               :   Helper
- * @Filename                       :  GHelper
- * @Description                   :  This helper is used to global functionalities of the framework.
- * @Author                          :   Cygnite Dev Team
- * @Copyright                     :  Copyright (c) 2013 - 2014,
- * @Link	                  :  http://www.cygniteframework.com
- * @Since	                  :  Version 1.0
+ * @Package                    :  Packages
+ * @Sub Packages               :  Helper
+ * @Filename                   :  GHelper
+ * @Description                :  This helper is used to global functionalist's of the framework.
+ * @Author                     :  Cygnite Dev Team
+ * @Copyright                  :  Copyright (c) 2013 - 2014,
+ * @Link	                   :  http://www.cygniteframework.com
+ * @Since	                   :  Version 1.0
  * @Filesource
- * @Warning                     :  Any changes in this library can cause abnormal behaviour of the framework
+ * @Warning                    :  Any changes in this library can cause abnormal behaviour of the framework
  *
  *
  */
 class GHelper
+{
+    /*
+    * $_POST   = array_map("clearSanity", $_POST);
+    * Strip html encoding out of a string, useful to prevent cross site scripting attacks
+    * Use this function in view page to display values into web page
+    */
+    public static function clearSanity($values)
     {
-        /*
-         * $_POST   = array_map("clearsanity", $_POST);
-         * Strip html encoding out of a string, useful to prevent cross site scripting attacks
-         * Use this function in view page to display values into web page
-         */
-        public static function clearsanity($values)
-        {
-                 (is_array($values)) ? $values = array_map("clearsanity", $values) : $values = htmlentities($values, ENT_QUOTES, 'UTF-8');
-             return $values;
+        $values = (is_array($values)) ?
+            array_map("clearSanity", $values) :
+            htmlentities($values, ENT_QUOTES, 'UTF-8');
+
+        return $values;
+    }
+
+    public static function display_errors($err_type, $err_header, $err_message, $err_file, $line_num = null, $debug = false)
+    {
+        Cygnite::loader()->Exceptions->handleExceptions($err_type, $err_header, $err_message, $err_file, $line_num, $debug);
+    }
+
+    public static function logError($messege, $error_code = "", $line_num = "")
+    {
+
+    }
+
+    public static function daysDiff($date1)
+    {
+        if (!$date1) {
+            $date1="0000-00-00 00:00:00";
         }
 
-        public static function  display_errors($err_type,$err_header,$err_message,$err_file,$line_num = null,$debug = false)
-        {
-                 Cygnite::loader()->Exceptions->handleExceptions($err_type, $err_header,$err_message, $err_file, $line_num,$debug);
+        if (preg_match("/(\d+)-(\d+)-(\d+)/", $date1, $f)) {
+            $time_val=mktime(0, 0, 0, $f[2], $f[3], $f[1]);
         }
+        $today = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+        $s = $today-$time_val;
+        $d = intval($s/86400);
 
-         public static function  logError($messege,$error_code ="",$line_num = "")
-        {
+        return $d;
+    }
 
-        }
+    public static function getSingleton()
+    {
+        return \Cygnite\BaseController::getInstance();
+    }
 
-         public static function  daysDiff($date1)
-        {
-                  if (!$date1) $date1="0000-00-00 00:00:00";
-
-                  if (preg_match("/(\d+)-(\d+)-(\d+)/",$date1,$f)) {
-                    $time_val=mktime(0,0,0,$f[2],$f[3],$f[1]);
-                  }
-                  $today=mktime(0,0,0,date("m"),date("d"),date("Y"));
-                  $s = $today-$time_val;
-                  $d = intval($s/86400);
-             return $d;
-        }
-
-         public static function  getSingleton()
-        {
-                return \Cygnite\CF_BaseController::app(CF_ENCRYPT_KEY);
-        }
-
-        public static function trace()
-        {
-                include str_replace('/','',APPPATH).DS.'errors'.DS.'debugtrace'.EXT;
-                $output= ob_get_contents();
-                ob_get_clean();
-                echo $output;
-                ob_end_flush();
-                ob_get_flush();
-        }
-
+    public static function trace()
+    {
+        include str_replace('/', '', APPPATH).DS.'errors'.DS.'debugtrace'.EXT;
+        $output= ob_get_contents();
+        ob_get_clean();
+        echo $output;
+        ob_end_flush();
+        ob_get_flush();
+    }
 }

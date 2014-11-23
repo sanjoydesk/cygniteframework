495bb722e59a022d98db7bced3d2c3e5d0a77053
Sanjoy Dey <sanjoy09@hotmail.com>
2014-11-24 02:18:28 +0530

    File Upload and Image Thumbnail added.
    Doc block updated.

--- vendor/cygnite/framework/src/Cygnite/Foundation/AutoLoader.php
+++ vendor/cygnite/framework/src/Cygnite/Foundation/AutoLoader.php
@@ -1,8 +1,18 @@
 <?php
+
+/**
+ * This file is part of the Cygnite package.
+ *
+ * (c) Sanjoy Dey <dey.sanjoy0@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Cygnite\Foundation;
 
 use Cygnite\Exception;
-use Cygnite\Common\FileExtensionFilter;
+use Cygnite\Common\File\FileExtensionFilter;
 
 if (!defined('CF_SYSTEM')) {
     exit('External script access not allowed');
